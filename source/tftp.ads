e0d861a97535e326d55bf151a3ef135eea0540f3
Maxim Reznik <reznikmm@gmail.com>
2024-07-12 17:03:46 +0300

    Add stub source files

---
+++ source/tftp.ads
@@ -0,0 +1,8 @@
+--  SPDX-FileCopyrightText: 2024 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+----------------------------------------------------------------
+
+package Tftp is
+
+end Tftp;
